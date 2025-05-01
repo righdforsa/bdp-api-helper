944a5b085cdb39eb31a91a96d40eba1e694d9eee
Christopher Peters <righdforsa@gmail.com>
2025-05-01 12:17:22 -0700

    bump version

--- bdp-api-helper.php
+++ bdp-api-helper.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: BDP API Helper
  * Description: Dynamically exposes BDP (Business Directory Plugin) fields for REST API usage and validates meta field updates.
- * Version: 1.1.0
+ * Version: 1.1.1
  * Author: Christopher Peters
  * License: MIT
  * Text Domain: bdp-api-helper
